70f3c1368b1d5f42afffbe30960be172a288ca60
mohanad <mohanad@localhost>
2026-04-10 16:18:47 +0200

    compatability issues

--- src/main/java/model/RBDict.java
+++ src/main/java/model/RBDict.java
@@ -1,50 +1,55 @@
 package model;
 
-
 public class RBDict {
+
     private RBNode<String> root;
-    private int height;
     private int size;
 
     public RBDict() {
         this.root = null;
-        this.height = 0;
         this.size = 0;
     }
-    
-    public boolean search(RBNode<String> root,String key ){
-        if(root==null)
+
+    public boolean search(RBNode<String> root, String key) {
+        if (root == null) {
             return false;
-        else if( root.getData().equalsIgnoreCase( key ) )
+        } else if (root.getData().equalsIgnoreCase(key)) {
             return true;
-        else if ( root.getData().compareToIgnoreCase(key)>0 )
-             return search(root.getLeft(),key);
-
-        else
-           return search(root.getRight(),key);
+        } else if (root.getData().compareToIgnoreCase(key) > 0) {
+            return search(root.getLeft(), key);
+        } else {
+            return search(root.getRight(), key);
+        }
 
     }
-    public void insert(String word){
-        
+
+    public void insert(RBNode<String> root, RBNode<String> node) {
+
     }
     
-    public int getHeight(RBNode<String> root){
-        if(root==null)
+    
+     public RBNode getRoot() {
+        return this.root;
+    }
+
+    public int getHeight(RBNode<String> root) {
+        if (root == null) {
             return 0;
-        int Lheight=getHeight( root.getLeft() );
-        int Rheight=getHeight( root.getRight() );
+        }
+        int Lheight = getHeight(root.getLeft());
+        int Rheight = getHeight(root.getRight());
 
-        return 1+ Math.max(Lheight,Rheight);
+        return 1 + Math.max(Lheight, Rheight);
     }
-    
-    public int getSize(){
+
+    public int getSize() {
         return this.size;
     }
 
-
     public int getBlackHeight(RBNode<String> root) {
-        if (root == null)
+        if (root == null) {
             return 0;
+        }
 
         int L = getBlackHeight(root.getLeft());
         int R = getBlackHeight(root.getRight());
@@ -53,9 +58,10 @@ public class RBDict {
             throw new RuntimeException("Red-Black Tree violated!");
         }
 
-        if (root.getColor() == Color.BLACK)
+        if (root.getColor() == NodeColor.BLACK) {
             return L + 1;
-        else
+        } else {
             return L;
+        }
     }
 }
